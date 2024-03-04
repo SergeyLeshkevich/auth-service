44fb6eebdcff3f29634721494e4677e093468b88
Sergey Leshkevich <s.leshkevichemail@gmail.com>
2024-03-04 12:31:42 +0300

    add AuthService class

---
+++ src/main/java/ru/clevertec/auth/service/AuthService.java
@@ -0,0 +1,12 @@
+package ru.clevertec.auth.service;
+
+
+import ru.clevertec.auth.entity.dto.auth.JwtRequest;
+import ru.clevertec.auth.entity.dto.auth.JwtResponse;
+
+public interface AuthService {
+
+    JwtResponse login(JwtRequest loginRequest);
+    JwtResponse refresh(String refreshToken);
+    JwtResponse validate(String token);
+}
