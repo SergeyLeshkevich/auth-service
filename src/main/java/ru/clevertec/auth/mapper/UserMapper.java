fd6e272ababf179d41bfa6aacd85199dba4d12b5
Sergey Leshkevich <s.leshkevichemail@gmail.com>
2024-03-04 12:28:57 +0300

    add UserMapper class

---
+++ src/main/java/ru/clevertec/auth/mapper/UserMapper.java
@@ -0,0 +1,25 @@
+package ru.clevertec.auth.mapper;
+
+import org.mapstruct.Mapper;
+import ru.clevertec.auth.entity.user.User;
+import ru.clevertec.auth.entity.dto.user.UserResponse;
+
+
+/**
+ * Mapper interface for converting between User entity and UserResponse DTO.
+ * This interface is used to abstract the conversion logic between the database entity and the data transfer object.
+ *
+ * @author Sergey Leshkevich
+ * @version 1.0
+ */
+@Mapper(componentModel = "spring")
+public interface UserMapper {
+
+    /**
+     * Converts a User entity to a UserResponse DTO.
+     *
+     * @param user the User entity to convert.
+     * @return the converted UserResponse DTO.
+     */
+    UserResponse toDto(User user);
+}
