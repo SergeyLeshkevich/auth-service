4cfa04ad3f71e79675cdaae74b4b9060314f9c7c
Sergey Leshkevich <s.leshkevichemail@gmail.com>
2024-03-04 12:41:14 +0300

    add UserControllerImpl class

---
+++ src/main/java/ru/clevertec/auth/controller/UserControllerImpl.java
@@ -0,0 +1,42 @@
+package ru.clevertec.auth.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import ru.clevertec.auth.entity.dto.user.UserRequest;
+import ru.clevertec.auth.entity.dto.user.UserResponse;
+import ru.clevertec.auth.entity.user.Role;
+import ru.clevertec.auth.service.UserViewService;
+
+
+@Validated
+//@CrossOrigin                                                                  //TODO
+@RestController
+@RequiredArgsConstructor
+public class UserControllerImpl implements UserController{
+
+    private final UserViewService userService;
+
+
+    @Override
+    public UserResponse update(Long id, UserRequest dto) {
+        return userService.updateUser(id, dto);
+    }
+
+    @Override
+    public UserResponse create(UserRequest dto,Role role) {
+        return userService.create(dto,role);
+    }
+
+
+    @Override
+    public UserResponse getById(Long id) {
+        return userService.getUserDtoById(id);
+    }
+
+
+    @Override
+    public void archivedById(Long id) {
+        userService.archive(id);
+    }
+}
