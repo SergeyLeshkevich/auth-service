373994b7b7389a0b0d07c8df08c0b8dd1e4789bd
Sergey Leshkevich <s.leshkevichemail@gmail.com>
2024-03-28 13:24:49 +0300

    refactor javadoc

--- src/main/java/ru/clevertec/auth/repository/RoleRepository.java
+++ src/main/java/ru/clevertec/auth/repository/RoleRepository.java
@@ -6,9 +6,21 @@ import ru.clevertec.auth.entity.user.Role;
 
 import java.util.Optional;
 
-
+/**
+ * Repository interface for Role entity.
+ * This interface extends JpaRepository, providing custom queries for Role entities.
+ *
+ * @author Sergey Leshkevich
+ * @version 1.0
+ */
 @Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
 
+    /**
+     * Finds a role by their name.
+     *
+     * @param name the name of the role to find.
+     * @return an Optional containing the found role or an empty Optional if not found.
+     */
     Optional<Role> findByName(String name);
 }
--- src/main/java/ru/clevertec/auth/service/impl/RoleServiceImpl.java
+++ src/main/java/ru/clevertec/auth/service/impl/RoleServiceImpl.java
@@ -7,13 +7,26 @@ import ru.clevertec.auth.repository.RoleRepository;
 import ru.clevertec.auth.service.RoleService;
 import ru.clevertec.exceptionhandlerstarter.exception.EntityNotFoundException;
 
-
+/**
+ * Implementation of role services.
+ * This class interacts with the RoleRepository to perform database operations.
+ *
+ * @author Sergey Leshkevich
+ * @version 1.0
+ */
 @Service
 @RequiredArgsConstructor
 public class RoleServiceImpl implements RoleService {
 
     private final RoleRepository roleRepository;
 
+    /**
+     * Retrieves a role by their name.
+     *
+     * @param name the name of the role to retrieve.
+     * @return the found Role object.
+     * @throws EntityNotFoundException if no role is found with the provided name.
+     */
     @Override
     public Role getByName(String name) {
         return roleRepository.findByName(name).orElseThrow(
